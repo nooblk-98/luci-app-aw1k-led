0f3ce9a296c0a7a9c0801c680d30f68e5d3ecac6
NoobLk <liyanagelsofficial@gmail.com>
2026-04-12 07:33:38 +0530

    Stop power LED blink immediately when night mode disabled

--- htdocs/luci-static/resources/view/aw1k-led/settings.js
+++ htdocs/luci-static/resources/view/aw1k-led/settings.js
@@ -410,10 +410,13 @@ return view.extend({
                 headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
                 body: nightBody
             }).then(function() {
+                var cmd = nightVals.night_enabled === '1'
+                    ? '/usr/bin/led-night-mode.sh check'
+                    : '/usr/bin/led-night-mode.sh off';
                 return fetch('/cgi-bin/luci/admin/system/aw1k-led/runcmd', {
                     method: 'POST',
                     headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-                    body: 'cmd=' + encodeURIComponent('/usr/bin/led-night-mode.sh check')
+                    body: 'cmd=' + encodeURIComponent(cmd)
                 });
             });
         });
